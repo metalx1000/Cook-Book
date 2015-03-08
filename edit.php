e43e170815568505a57c07b5281d86963b297404
metalx1000 <github@filmsbykris.com>
2015-03-08 15:01:00 -0400

    Moved nav to seperate file

--- edit.php
+++ edit.php
@@ -83,28 +83,7 @@
 
 </head>
 <body>
-    <!-- Fixed navbar -->
-    <nav class="navbar navbar-default navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">My Cook Book</a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="index.php">Home</a></li>
-            <li><a href="https://github.com/metalx1000/Cook-Book" target="_blank">Project Page</a></li>
-            <li><a href="http://filmsbykris.com" target="_blank">Contact</a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </nav>
-
+  <?php include("nav.php");?>
   <div class="container">
     <div id="recipe_create">
   <!--    <h2>Recipe Form</h2>-->
