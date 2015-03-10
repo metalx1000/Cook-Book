670459f67179dc154c1fdf3192aeb860df56e008
metalx1000 <github@filmsbykris.com>
2015-03-10 10:03:44 -0400

    added print button

--- list.php
+++ list.php
@@ -59,6 +59,9 @@
       font-size: 200%;
 
     }
+    #print{
+      display: none;
+    }
   </style>
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
