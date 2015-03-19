2fb29be0458b469aa360c97f7d55e5c40006e240
metalx1000 <github@filmsbykris.com>
2015-03-19 10:29:16 -0400

    added favicon

--- list.php
+++ list.php
@@ -1,6 +1,7 @@
 <html>
 <head>
   <title>Cook Book Search</title>
+  <link rel="icon" type="image/png" href="fav.png">
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
